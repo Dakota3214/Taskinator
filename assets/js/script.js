f1163455319780fbc2602e1ca513ca3a72954c26
Dakota Donaldson <dakota.donaldson17@icloud.com>
2021-08-24 16:00:55 -0500

    added ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var buttonE1 = document.querySelector("#save-task");
+// console.log(buttonE1);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task."
+    tasksToDoEl.appendChild(listItemEl);
+} 
+
+buttonE1.addEventListener("click", createTaskHandler);
+
+
+// var btn = document.querySelector("#save-task");
+// console.dir(btn);
